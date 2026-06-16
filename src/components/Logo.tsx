16e8b5e5a63c83400c5d6406897bbffa3cddfd87
William Mock <william.mock@gmail.com>
2026-06-16 15:55:41 -0400

    Use updated visible logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,12 +5,12 @@ export function Logo() {
   return (
     <Link href="/" className="focus-ring block" aria-label="Centerline Roofing & Exteriors home">
       <Image
-        src="/brand/centerline-horizontal.svg"
+        src="/brand/centerline-icon.svg"
         alt="Centerline Roofing & Exteriors"
         width={250}
-        height={69}
+        height={256}
         priority
-        className="h-12 w-[218px] object-contain sm:h-14 sm:w-[250px]"
+        className="h-14 w-14 object-contain sm:h-16 sm:w-16"
       />
     </Link>
   );
